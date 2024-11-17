30267637a142ad56773f945bae97654dd9caaf6e
AuziMan <austintdriver@gmail.com>
2024-11-16 23:11:14 -0700

    Added dynamic team names for squad page

--- react-frontend/src/games/teamGames.jsx
+++ react-frontend/src/games/teamGames.jsx
@@ -6,6 +6,7 @@ import GameCards from '../components/GameCards';
 function TeamGames() {
     const { teamId } = useParams();
     const [games, setGames] = useState([]);
+    const [teamName, setTeamName] = useState(''); // State for team name
     const [isLoading, setIsLoading] = useState(true); // New isLoading state
     const [error, setError] = useState(null);
 
@@ -19,6 +20,19 @@ function TeamGames() {
                 }
                 const data = await response.json();
                 setGames(data);
+
+                // Get Team Name from team Id
+
+                if(data.length > 0) {
+                    const firstGame = data[0];
+                    const {home, away} = firstGame.fixture_teams
+                    if(home.id === parseInt(teamId)){
+                        setTeamName(home.name)
+                    } else if (away.id === parseInt(teamId)){
+                        setTeamName(away.name)
+                    }
+                }
+
                 setIsLoading(false);
             } catch (error) {
                 setError(error.message);
@@ -49,7 +63,7 @@ function TeamGames() {
     return (
         <div>
             <div className="games-title">
-                <h1>Past Games for </h1>
+                <h1>Past Games for {teamName || 'Team'} </h1>
             </div>
             <div className="center-wrapper">
                 <div className="team-games-container">
